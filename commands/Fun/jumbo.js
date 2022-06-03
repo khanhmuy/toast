e1f6c86a2691bc355a7baae4d6ca35169986deb5
khanhmuy <maikhanhhuy0608@outlook.com>
2022-06-03 21:09:15 +0700

    jumbo command reply changes

--- commands/Fun/jumbo.js
+++ commands/Fun/jumbo.js
@@ -1,6 +1,5 @@
 const {MessageEmbed} = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const Vibrant = require('node-vibrant')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('jumbo')
@@ -12,7 +11,6 @@ module.exports = {
                 .setRequired(true);
         }),
     async execute(interaction) {
-        console.log(interaction);  
         const input = interaction.options.getString('emoji');
         const msg = input.match(/<a?:.+:\d+>/gm);
         let url = '';
@@ -22,12 +20,7 @@ module.exports = {
             url = "https://cdn.discordapp.com/emojis/" + emoji[1] + ".gif?v=1?size=1000";
         } 
         if (url) {
-            const color = await Vibrant.from(url).getPalette();
-            embed = new MessageEmbed()
-                .setColor(color.Vibrant.hex)
-                .setAuthor(interaction.user.username, `${interaction.user.displayAvatarURL({ dynamic: true })}?size=1024`)
-                .setImage(url)
-            interaction.reply({ embeds: [embed] });
+            interaction.reply({ files: [url] });
         } if (!url) {
             interaction.reply('Invalid emoji!, might be a Unicode emoji or an external / animated emoji that you don\'t have access to. ~~(get nitro you cheap mf)~~');
         }
