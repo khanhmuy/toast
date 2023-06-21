7c7a9ef6333c5ddfa19b0b79f79832c21e9642b9
khanhmuy <maikhanhhuy0608@outlook.com>
2023-06-21 21:47:25 +0700

    remove binny waifu (doesn't work lol)

--- events/ready.js
+++ events/ready.js
@@ -6,26 +6,5 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Connection established (${client.ws.ping}ms). Logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
-		async function dailyWaifu() {
-			const date = new Date();
-			const hour = date.toLocaleString('en-AU', {timeZone: 'Australia/Queensland', hour: '2-digit', hour12: false });
-			const minute = date.toLocaleString('en-AU', {timeZone: 'Australia/Queensland', minute: '2-digit' });
-			if (hour === '12' && minute === '00') {
-				const res = await axios.get('https://api.waifu.pics/sfw/waifu')
-				let color = null
-				color = await Vibrant.from(res.data.url).getPalette()
-				color = color.Vibrant.hex
-				const embed = new EmbedBuilder()
-					.setColor(color)
-					.setTimestamp()
-					.setImage(res.data.url)
-					.setURL(res.data.url)
-				try {
-					const binnyChannel = client.channels.cache.get('911174855821496330');
-					binnyChannel.send({ content: '<@&911172278090367007>', embeds: [embed] });
-				} catch (err) {}
-			}
-		}
-		setInterval(dailyWaifu, 10000);
 	},
 };
