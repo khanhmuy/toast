616e769e6bbca43d64d53837b6628b10495007e2
khanhmuy <maikhanhhuy0608@outlook.com>
2023-07-04 06:15:41 +0700

    fix fxmedia

--- events/fxmedia.js
+++ events/fxmedia.js
@@ -4,15 +4,17 @@ module.exports = {
         if (interaction.author.bot == true || interaction.content == undefined) return;
         const exec = interaction.client.data.get(`guild.${interaction.guildId}.fxmedia`);
         if (exec === undefined) return;
-        const twitter = /^http(?:s)?:\/\/(.*)twitter\.com\//
-        const facebook = /^http(?:s)?:\/\/(.*)(www)?(.*)facebook\.com\//
+        const twitter = /^http(?:s)?:\/\/(.*)\btwitter\b\.com\//
+        var twRegExp = new RegExp(twitter, 'g');
+        const facebook = /^http(?:s)?:\/\/(.*)(www)?(.*)\bfacebook\b\.com\//
+        var fbRegExp = new RegExp(facebook, 'g');
         if (twitter.test(interaction.content) == true) {
             interaction.suppressEmbeds(true);
-            interaction.reply({content: interaction.content.replace(/twitter.com/g, 'vxtwitter.com'), allowedMentions: {repliedUser: false}})
+            interaction.reply({content: interaction.content.replace(twRegExp, 'https://vxtwitter.com/'), allowedMentions: {repliedUser: false}})
         }
         if (facebook.test(interaction.content) == true) {
             interaction.suppressEmbeds(true);
-            interaction.reply({content: interaction.content.replace(/^http(?:s)?:\/\/(.*)(www)?(.*)facebook\.com\//g, 'https://fxfacebook.beerpsi.tech/'), allowedMentions: {repliedUser: false}})
+            interaction.reply({content: interaction.content.replace(fbRegExp, 'https://fxfacebook.beerpsi.tech/'), allowedMentions: {repliedUser: false}})
         }
     },
 }
