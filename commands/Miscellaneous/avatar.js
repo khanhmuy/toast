036a6494ced924ba6bbb69ec139971d712ab6e11
khanhmuy <maikhanhhuy0608@outlook.com>
2024-10-05 01:52:21 +0700

    add guild pfp for avatar command

--- commands/Miscellaneous/avatar.js
+++ commands/Miscellaneous/avatar.js
@@ -9,16 +9,31 @@ module.exports = {
                 .setName('user')
                 .setDescription('The user to get the avatar of.')
                 .setRequired(false);
+        })
+        .addBooleanOption(option => {
+            return option
+                .setName('server')
+                .setDescription('Whether to get the user\'s server avatar')
+                .setRequired(false);
         }),
     async execute(interaction) {
         await interaction.deferReply();
         try {
             let {jpeg, png, webp, embed, color} = '';
+            const server = interaction.options.getBoolean('server');
             if (!interaction.options.getUser('user')) {
-                const rawLink = interaction.user.displayAvatarURL({ format: 'png', size: 1024 });
-                jpeg = rawLink.slice(0, 86) + '.jpg?size=1024';
-                png = rawLink.slice(0, 86) + '.png?size=1024';
-                webp = rawLink.slice(0, 86) + '.webp?size=1024';
+                let rawLink = '';
+                if (server === true) {
+                    rawLink = interaction.member.displayAvatarURL({ format: 'png', size: 1024 });
+                    jpeg = rawLink.slice(0, 118) + '.jpg?size=1024';
+                    png = rawLink.slice(0, 118) + '.png?size=1024';
+                    webp = rawLink.slice(0, 118) + '.webp?size=1024';
+                } else {
+                    rawLink = interaction.user.displayAvatarURL({ format: 'png', size: 1024 });
+                    jpeg = rawLink.slice(0, 86) + '.jpg?size=1024';
+                    png = rawLink.slice(0, 86) + '.png?size=1024';
+                    webp = rawLink.slice(0, 86) + '.webp?size=1024';
+                }
                 color = await Vibrant.from(png).getPalette();
                 embed = new EmbedBuilder()
                     .setTitle(`Avatar of ${interaction.user.username}`)
@@ -26,14 +41,23 @@ module.exports = {
                         {name: 'Download as', value: '[jpeg](' + jpeg + ') | [png](' + png + ') | [webp](' + webp + ')'}
                     ])
                     .setColor(color.Vibrant.hex)
-                    .setImage(interaction.user.displayAvatarURL({format:'png', size: 1024, dynamic: true}))
+                    .setImage(png)
                     .setURL(png);
             } else {
                 const user = interaction.options.getUser('user');
-                const rawLink = user.displayAvatarURL({ format: 'png', size: 1024 });
-                jpeg = rawLink.slice(0, 86) + '.jpg?size=1024';
-                png = rawLink.slice(0, 86) + '.png?size=1024';
-                webp = rawLink.slice(0, 86) + '.webp?size=1024';
+                let rawLink = '';
+                if (server === true) {
+                    const member = interaction.guild.members.cache.get(interaction.options.getUser('user').id)
+                    rawLink = member.displayAvatarURL({ format: 'png', size: 1024 });
+                    jpeg = rawLink.slice(0, 118) + '.jpg?size=1024';
+                    png = rawLink.slice(0, 118) + '.png?size=1024';
+                    webp = rawLink.slice(0, 118) + '.webp?size=1024';
+                } else {
+                    rawLink = user.displayAvatarURL({ format: 'png', size: 1024 });
+                    jpeg = rawLink.slice(0, 86) + '.jpg?size=1024';
+                    png = rawLink.slice(0, 86) + '.png?size=1024';
+                    webp = rawLink.slice(0, 86) + '.webp?size=1024';
+                }
                 color = await Vibrant.from(png).getPalette();
                 embed = new EmbedBuilder()
                     .setTitle(`Avatar of ${user.username}`)
@@ -41,7 +65,7 @@ module.exports = {
                         {name: 'Download as', value: '[jpeg](' + jpeg + ') | [png](' + png + ') | [webp](' + webp + ')'}
                     ])
                     .setColor(color.Vibrant.hex)
-                    .setImage(user.displayAvatarURL({ format: 'png', size: 1024, dynamic: true }))
+                    .setImage(png)
                     .setURL(png);
             };
             await interaction.editReply({embeds: [embed]});
