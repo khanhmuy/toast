5b29871cd935599c026c9860e3349abd4ac4bee6
khanhmuy <maikhanhhuy0608@outlook.com>
2024-11-06 07:07:27 +0700

    fix wiki command

--- commands/Services/wiki.js
+++ commands/Services/wiki.js
@@ -15,32 +15,34 @@ module.exports = {
         await interaction.deferReply();
         try {
             const query = interaction.options.getString('query');
-            const res = await wiki.summary(query);
-            if (res === null) {
-                interaction.editReply({content: 'No results found.'});
+            try {
+                const res = await wiki.page(query);
+            } catch (error) {
+                interaction.editReply({content: 'No results found!', ephemeral: true});
                 return;
             }
+            const summary = await res.summary();
             let color = null;
             let thumbnail = '';
             try {
-                thumbnail = res.thumbnail.source;
-                color = await Vibrant.from(res.thumbnail.source).getPalette();
+                thumbnail = summary.thumbnail.source;
+                color = await Vibrant.from(summary.thumbnail.source).getPalette();
                 color = color.Vibrant.hex;
             } catch {
                 thumbnail = 'https://en.wikipedia.org/static/images/project-logos/enwiki.png';
                 color = '#89b4fa';
             }
             let extract = '';
-            if (res.extract.length > 1024) {
-                extract = res.extract.substring(0, 1018) + '(...)';
+            if (summary.extract.length > 1024) {
+                extract = summary.extract.substring(0, 1018) + '(...)';
             } else {
-                extract = res.extract;
+                extract = summary.extract;
             }
             const embed = new EmbedBuilder()
-                .setTitle(res.title)
-                .setURL(res.content_urls.desktop.page)
+                .setTitle(summary.title)
+                .setURL(summary.content_urls.desktop.page)
                 .setThumbnail(thumbnail)
-                .setDescription(res.description)
+                .setDescription(summary.description)
                 .addFields([
                     {name: '\u200b', value: extract}
                 ])
